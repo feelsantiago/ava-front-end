d0c999ac40551bb4ed36aca4057fc795c5dc5d8d
Filipe Santiago <filipe@upplify.com>
2019-09-15 10:21:25 -0300

    add logout button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,12 +7,26 @@
  */
 
 import React, { FunctionComponent } from 'react';
+import useRouter from 'use-react-router';
+
 import { DashboardHeader } from '../../assets/styles/layouts/Header';
+import authService from '../../services/auth-service';
 
 const Header: FunctionComponent = () => {
+	const { history } = useRouter();
+
 	return (
 		<div id="app-header">
-			<DashboardHeader />
+			<DashboardHeader>
+				<a
+					onClick={() => {
+						authService.logout();
+						history.push('/login');
+					}}
+				>
+					Logout
+				</a>
+			</DashboardHeader>
 		</div>
 	);
 };
